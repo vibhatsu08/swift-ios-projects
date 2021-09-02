44073cadfb3915b18e34f56b798db9951f7cd345
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-03 02:01:20 +0530

    Landmarks app iOS
    iOS Landmarks app made using Swift and SwiftUI. Complete tutorial on Apple's Developer website.

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Vedant Mistry on 02/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
