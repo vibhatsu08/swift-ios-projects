fbc315be89cce18d8ee2a886921e275fc09bb05a
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-10-22 13:18:10 +0530

    countdown timer in swift

--- appTimer/appTimer/ViewController.swift
+++ appTimer/appTimer/ViewController.swift
@@ -11,22 +11,29 @@ class ViewController: UIViewController {
 
     @IBOutlet var countdownLabel: UILabel!
     
-    var timer: Timer?
-    var timeLeft = 60
+    var timer: Timer? //create a variable for the timer with an optional value.
+    var timeLeft = 0 //the time for the timer
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func firstButtonPressed(_ sender: UIButton) {
+        countdownLabel.text = sender.currentTitle
+        timeLeft = 60
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    }
+    @IBAction func secondButtonPressed(_ sender: UIButton) {
+        countdownLabel.text = sender.currentTitle
+        timeLeft = 120
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
     }
     
+    
     @objc func update(){
-        timeLeft -= 1
-        countdownLabel.text = "\(timeLeft)"
         
         if timeLeft <= 0{
             timer?.invalidate()
             timer = nil
         }
+        timeLeft -= 1
+        countdownLabel.text = "\(timeLeft)"
     }
 
 
